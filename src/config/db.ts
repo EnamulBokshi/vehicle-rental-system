08a1f16354823ca303141ec1788348f36215519f
EnamulBokshi <enamulbokshi@gmail.com>
2025-12-05 02:32:12 +0600

    update: updated the database and added users, vehicles, and bookings tables

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,3 +1,4 @@
+import { Defaults } from './../../node_modules/@types/pg/index.d';
 import {Pool} from 'pg'
 import config from '.'
 
@@ -9,8 +10,42 @@ export const pool = new Pool({
 const initDB = async()=>{
     try {
         await pool.query(`
-            
+            CREATE TABLE IF NOT EXISTS users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50) NOT NULL,
+            email VARCHAR(100) UNIQUE,
+            password TEXT NOT NULL CHECK (LENGTH(password) >= 6),
+            phone VARCHAR(16) NOT NULL,
+            role VARCHAR(20) NOT NULL  CHECK (role IN ('admin','customer')),
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            )
             `)
+        await pool.query(`
+                CREATE TABLE IF NOT EXISTS vehicles(
+                id SERIAL PRIMARY KEY,
+                vehicle_name VARCHAR(100) NOT NULL,
+                type VARCHAR(10) NOT NULL CHECK(type IN('car','bike','van','SUV')),
+                registration_number VARCHAR(20) NOT NULL UNIQUE,
+                daily_rent_price INT NOT NULL CHECK (daily_rent_price > 0),
+                availability_status VARCHAR(10) NOT NULL DEFAULT 'available' CHECK(availability_status IN ('available','booked')),
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW()
+                )
+            `)
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS bookings(
+                id SERIAL PRIMARY KEY,
+                customer_id INT REFERENCES users(id) ON DELETE CASCADE,
+                vehicle_id INT REFERENCES vehicles(id) ON DELETE CASCADE,
+                rent_start_date DATE NOT NULL,
+                rent_end_date DATE NOT NULL CHECK(rent_end_date > rent_start_date),
+                total_price NUMERIC(10,2) NOT NULL CHECK(total_price > 0),
+                status VARCHAR(10) NOT NULL CHECK(status IN('active', 'cancelled', 'returned')),
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOE()
+            )
+            `);
     } catch (error:any) {
         console.log("Database initialization failed: ", error.message)
     }
